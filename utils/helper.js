746aebf2f63605a033aa30f4c5dc1177ec9d6612
Billy Saysavath <bisaysavath@gmail.com>
2024-06-18 23:10:10 -0700

    Correct time format

--- utils/helper.js
+++ utils/helper.js
@@ -1,53 +1,53 @@
 const convertHourCodeToClockHour = (index) => {
   switch (index) {
     case 0:
-      return "12:00 am";
+      return "12:00am";
     case 1:
-      return "1:00 am";
+      return "1:00am";
     case 2:
-      return "2:00 am";
+      return "2:00am";
     case 3:
-      return "3:00 am";
+      return "3:00am";
     case 4:
-      return "4:00 am";
+      return "4:00am";
     case 5:
-      return "5:00 am";
+      return "5:00am";
     case 6:
-      return "6:00 am";
+      return "6:00am";
     case 7:
-      return "7:00 am";
+      return "7:00am";
     case 8:
-      return "8:00 am";
+      return "8:00am";
     case 9:
-      return "9:00 am";
+      return "9:00am";
     case 10:
-      return "10:00 am";
+      return "10:00am";
     case 11:
-      return "11:00 am";
+      return "11:00am";
     case 12:
-      return "12:00 pm";
+      return "12:00pm";
     case 13:
-      return "1:00 pm";
+      return "1:00pm";
     case 14:
-      return "2:00 pm";
+      return "2:00pm";
     case 15:
-      return "3:00 pm";
+      return "3:00pm";
     case 16:
-      return "4:00 pm";
+      return "4:00pm";
     case 17:
-      return "5:00 pm";
+      return "5:00pm";
     case 18:
-      return "6:00 pm";
+      return "6:00pm";
     case 19:
-      return "7:00 pm";
+      return "7:00pm";
     case 20:
-      return "8:00 pm";
+      return "8:00pm";
     case 21:
-      return "9:00 pm";
+      return "9:00pm";
     case 22:
-      return "10:00 pm";
+      return "10:00pm";
     case 23:
-      return "11:00 pm";
+      return "11:00pm";
     default:
       return "Unknown Time";
   }
